0bf9494061be5efe95f93ef0de35eb7166ba4461
wbmins <wbmins@qq.com>
2026-06-28 12:49:39 +0800

    delete unnecessary lib folders

--- lib-multisrc/greenshit/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    alias(kei.plugins.multisrc)
-}
-
-keiyoushi {
-    baseVersionCode = 10
-    libVersion = "1.4"
-}
--- lib-multisrc/vercomics/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    alias(kei.plugins.multisrc)
-}
-
-keiyoushi {
-    baseVersionCode = 3
-    libVersion = "1.4"
-}
