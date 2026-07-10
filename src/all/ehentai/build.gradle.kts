a84e2460db4f9a2984e21ed671d52e13eb2a67dc
wbmins <187116153+wbmins@users.noreply.github.com>
2026-07-10 15:35:03 +0800

    auto build v1.4.28 [skip ci]

--- src/all/ehentai/build.gradle.kts
+++ src/all/ehentai/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 keiyoushi {
     name = "E-Hentai"
     className = "EHFactory"
-    versionCode = 27
+    versionCode = 28
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
     baseUrl = "https://e-hentai.org"
